748526cf0e1723e6bd5e17e9b41b9e4cd90c806f
Dig <dig.s.vargas@gmail.com>
2014-01-28 14:22:31 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigPortfolio::Application.config.secret_token = '92bef182521b233e217765698dcaae7a70192c0074cfa7b6f2f045293143bf72b81e18a10ebfce5d635a6fd9fdededdfd3e4e88a640575cdd5b97e6cc682fd2b'
